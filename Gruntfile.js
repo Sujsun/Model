5a3f0374d78e562b81514793188413be2ecbe294
Sundarasan <Sundarasan@tenmiles.com>
2016-03-16 14:28:14 +0530

    Added minification grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,18 @@
 
+function getPluginBanner () {
+  var pluginBanner = '';
+  pluginBanner += '/**\n';
+  pluginBanner += ' * Plugin: Model\n';
+  pluginBanner += ' * Author: Sundarasan Natarajan\n';
+  pluginBanner += ' * GIT: https://github.com/Sujsun/Model.git\n';
+  pluginBanner += ' * Version: 0.0.1\n';
+  pluginBanner += ' * Date: ' + new Date().toUTCString() + '\n';
+  pluginBanner += ' */\n';
+  return pluginBanner;  
+}
+
+var pluginBanner = getPluginBanner();
+
 module.exports = function(grunt) {
   grunt.initConfig({
 
@@ -10,13 +24,30 @@ module.exports = function(grunt) {
         src: ['./index.js'],
         dest: 'dist/model.js'
       },
-    }
+    },
+
+    uglify: {
+      dist: {
+        options: {
+          banner: pluginBanner
+        },
+        files: {
+          'dist/model.min.js': ['dist/model.js']
+        }
+      }
+    },
 
   });
 
   grunt.loadNpmTasks('grunt-watchify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('build', [
+    'watchify',
+    'uglify',
+  ]);
 
   grunt.registerTask('default', [
-    'watchify',
+    'build',
   ]);
 };
